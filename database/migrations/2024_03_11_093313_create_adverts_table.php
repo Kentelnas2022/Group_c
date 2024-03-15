3681393af7773b18bd2475e200905725f350f326
unknown <juvyaballe.202201044@gmail.com>
2024-03-15 20:11:42 +0800

    first commit

---
+++ database/migrations/2024_03_11_093313_create_adverts_table.php
@@ -0,0 +1,34 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('adverts', function (Blueprint $table) {
+            $table->id('Post_ID');
+            $table->string('status');
+            $table->string('identify_name');
+            $table->string('description');
+            $table->string('Contact_no');
+            $table->string('location');
+            $table->string('addphoto');
+            $table->rememberToken();
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('adverts');
+    }
+};
